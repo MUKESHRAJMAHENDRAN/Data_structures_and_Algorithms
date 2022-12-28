3adc6419d066b57179f129310b26d6ee8b2f9a0f
mukeshrajmahendran <mukeshrajmahendran@gmail.com>
2022-12-28 19:01:05 +0530

    Even or Odd Program in java

---
+++ dsa_java/src/com/mukesh/Odd_Even.java
@@ -0,0 +1,17 @@
+package com.mukesh;
+
+import java.util.Scanner;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = in.nextInt();
+        if (num % 2 == 0){
+            System.out.println(num + " is an even number");
+        }
+        else{
+            System.out.println(num + " is an odd number");
+        }
+    }
+}
