dd7438e03b1e579597f447ed8512242c65e16e17
Mukeshraj <mukeshrajmahendran@gmail.com>
2023-01-24 23:51:50 +0530

    Pass value

---
+++ dsa_java_module_2/code/dsa_module_2/src/com/mukesh/Value.java
@@ -0,0 +1,16 @@
+package com.mukesh;
+
+public class Value {
+    public static void main(String[] args) {
+        System.out.println("Mukesh");
+        String name_1 = "Lokesh";
+        System.out.println(name_1);
+        name("Devi");
+        System.out.println(name_1);
+        int a[0]
+    }
+    static void name(String naam){
+        naam = "Lok";
+        System.out.println(naam);
+    }
+}
